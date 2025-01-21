efe80989bdf7ba344ca1e35e1667586c7d2f7a48
Christian Putzu <christian.putzu.job@gmail.com>
2025-01-21 16:01:31 +0100

    Update keys

--- app.py
+++ app.py
@@ -72,6 +72,7 @@ def create_med_plan_from_img(patient_name, image_file):
 
 if __name__=='__main__':
     load_dotenv(".env")
+    # google_api_key = os.getenv("GOOGLE_API_KE")
     google_api_key = st.secrets["google"]["api_key"]
     genai.configure(api_key=google_api_key)
     
